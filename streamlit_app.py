51d6df168b8b077050fce9c842fc569a9659c9d9
ChunJieTan <166084379+ChunJieTan@users.noreply.github.com>
2026-03-28 02:08:16 +0000

    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,12 @@ import numpy as np
 model = load('linear_regression_model.joblib')
 
 # Create a simple user input
-#user_input = st.number_input('Enter house size:', min_value=100, max_value=10000, step=50)
+user_input = st.number_input('Enter house size:', min_value=100, max_value=10000, step=50)
 
 # Reshape the input for the model
-#input_array = np.array([user_input]).reshape(-1, 1)
+input_array = np.array([user_input]).reshape(-1, 1)
 
 # Predict the house price
-#if st.button('Predict Price'):
-#   predicted_price = model.predict(input_array)
-#  st.write(f"The predicted house price is: ${predicted_price[0]:.2f}")
+if st.button('Predict Price'):
+   predicted_price = model.predict(input_array)
+  st.write(f"The predicted house price is: ${predicted_price[0]:.2f}")
